14dfedd06ca9826cd7307d2668523d1c5e3def5a
Glorzo <thisaraeranga@gmail.com>
2022-03-07 21:33:25 -0600

    created CREATE,READ, UPDATE and DELETE operations

--- server.js
+++ server.js
@@ -20,18 +20,49 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
-
-
-
 // express test (not needed after confirming express server connection)
 // app.get('/', (req,res) => {
 //     res.json({
 //         message: 'Hello World!'
 //     });
 // });
+
+
+
+
+// // GET all the candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// })
+
+// // GET a single candidate by ID
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err,row) => {
+//     if(err) {
+//         console.log(err);
+//     } 
+//     console.log(row);
+// })
+
+// // DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err,result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
+
+// create a candidate
+const sql = `INSERT INTO candidates (id, first_name, Last_name, industry_connected)
+            VALUES (?, ?, ?, ?)`;
+const params = [1, 'Ronals', 'Firbank', 1];
+
+db.query(sql,params,(err,result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
 
 
 
