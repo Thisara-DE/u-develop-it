6868567f68bc27e07b09c97f8eb40ac7bb47a74c
Glorzo <thisaraeranga@gmail.com>
2022-03-07 20:30:44 -0600

    db connected to the server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,24 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username
+        user: 'root',
+        password: '6e0*kXsaIn@^**Sc',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
+
+
 
 // express test (not needed after confirming express server connection)
 // app.get('/', (req,res) => {
